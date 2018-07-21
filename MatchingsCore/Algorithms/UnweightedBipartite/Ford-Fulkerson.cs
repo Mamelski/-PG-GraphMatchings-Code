1b7246e3cc03c821f30f6986f59675b2af7ed939
Mamelski <jakub.mamelski@outlook.com>
2018-07-21 19:43:19 +0200

    Ford-Fulkerson skelet

--- MatchingsCore/Algorithms/UnweightedBipartite/Ford-Fulkerson.cs
+++ MatchingsCore/Algorithms/UnweightedBipartite/Ford-Fulkerson.cs
@@ -8,14 +8,35 @@
     public class Ford_Fulkerson
     {
         /// <summary>
+        /// The source id.
+        /// </summary>
+        private int sourceId = -1;
+
+        /// <summary>
+        /// The sink id.
+        /// </summary>
+        private int sinkId = -2;
+        
+        /// <summary>
         /// The get maximum matching.
         /// </summary>
         /// <param name="graph">
         /// The graph.
         /// </param>
-        public void GetMaximumMatching(AdjacencyListGraph graph)
+        public void GetMaximumMatching(Graph graph)
         {
+            this.AddSourceAndSink(graph);
+        }
 
+        /// <summary>
+        /// The add source and sink.
+        /// </summary>
+        /// <param name="graph">
+        /// The graph.
+        /// </param>
+        private void AddSourceAndSink(Graph graph)
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
