ec4371cd1321cf071f055eb5f3b596c29a61e70e
Mamelski <jakub.mamelski@outlook.com>
2018-07-23 16:53:29 +0200

    Edge added

---
+++ MatchingsCore/GraphRepresentation/Edge.cs
@@ -0,0 +1,18 @@
+﻿namespace MatchingsCore.GraphRepresentation
+{
+    /// <summary>
+    /// The edge.
+    /// </summary>
+    public class Edge
+    {
+        /// <summary>
+        /// Gets or sets the weigth.
+        /// </summary>
+        public int Weigth { get; set; }
+
+        /// <summary>
+        /// Gets or sets the destination.
+        /// </summary>
+        public Node Destination { get; set; }
+    }
+}
