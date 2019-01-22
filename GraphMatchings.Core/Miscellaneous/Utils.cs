3c19d30f178e210a0855bba4bb16ca674e74fa87
Jakub Mamelski <jakub.mamelski@outlook.com>
2019-01-22 14:35:34 +0100

    Restore old projects as .netcore

---
+++ GraphMatchings.Core/Miscellaneous/Utils.cs
@@ -0,0 +1,92 @@
+﻿namespace MatchingsCore.Miscellaneous
+{
+    using System.Collections.Generic;
+
+    using MatchingsCore.GraphRepresentation;
+
+    /// <summary>
+    /// The utils.
+    /// </summary>
+    public class Utils
+    {
+        /// <summary>
+        /// The number of nodes.
+        /// </summary>
+        private int numberOfNodes;
+
+        /// <summary>
+        /// The graph.
+        /// </summary>
+        private AdjacencyMatrixGraph graph;
+
+        /// <summary>
+        /// The is bipartite.
+        /// </summary>
+        /// <param name="graph">
+        /// The graph.
+        /// </param>
+        /// <returns>
+        /// The <see cref="bool"/>.
+        /// </returns>
+        public bool ColortGraphAndCheckIfItIsBipartite(ref AdjacencyMatrixGraph graph)
+        {
+            this.numberOfNodes = graph.Nodes.Count;
+            this.graph = graph;
+            var firstNode = graph.Nodes[0];
+
+            var isGraphBipartite = this.DFSWithTwoColors(ref firstNode, 0);
+
+            // TODO sprawdz czy cykl
+            // TODO co jeśli nie graf
+            return isGraphBipartite;
+        }
+
+        /// <summary>
+        /// The dfs with two colors.
+        /// </summary>
+        /// <param name="node">
+        /// The node.
+        /// </param>
+        /// <param name="currentColor">
+        /// The currentColor.
+        /// </param>
+        /// <returns>
+        /// The <see cref="bool"/>.
+        /// </returns>
+        private bool DFSWithTwoColors(ref Node node, int currentColor)
+        {
+            node.Visited = true;
+
+            var neighboursColor = currentColor == 0 ? 1 : 0;
+
+            for (var i = 0; i < this.numberOfNodes; ++i)
+            {
+                if (this.graph.Matrix[node.Id][i] == 0)
+                {
+                    continue;
+                }
+
+                var neighour = this.graph.Nodes[i];
+
+                if (!neighour.Visited)
+                {
+                    neighour.Color = neighboursColor;
+
+                    if (!this.DFSWithTwoColors(ref neighour, neighboursColor))
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    if (node.Color == neighour.Color)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ GraphMatchings.Core/Serializers/AdjacencyListGraphSerializer.cs
@@ -0,0 +1,189 @@
+﻿//namespace MatchingsCore.Serializers
+//{
+//    using System.Collections.Generic;
+//    using System.IO;
+//    using System.Linq;
+
+//    using MatchingsCore.GraphRepresentation;
+
+//    /// <summary>
+//    ///     The adjacency list serializer.
+//    /// </summary>
+//    public class AdjacencyListGraphSerializer
+//    {
+//        /// <summary>
+//        ///     The deserialize.
+//        /// </summary>
+//        /// <param name="pathToFile">
+//        ///     The path to file.
+//        /// </param>
+//        /// <returns>
+//        ///     The <see cref="Graph" />.
+//        /// </returns>
+//        public Graph Deserialize(string pathToFile)
+//        {
+//            this.CheckIfFileExists(pathToFile);
+
+//            AdjacencyListGraph graph;
+
+//            using (var reader = File.OpenText(pathToFile))
+//            {
+//                var numberOfNodes = this.ParseNumberOfNodesFromFile(reader, pathToFile);
+
+//                graph = new AdjacencyListGraph(numberOfNodes);
+
+//                for (var i = 0; i < numberOfNodes; ++i)
+//                {
+//                    var line = this.ReadLineFromFile(reader, i, numberOfNodes, pathToFile);
+
+//                    var nodeId = this.ParseNodeIdFromLine(line, pathToFile);
+
+//                    var neighbours = this.ParseNeighboursFromFile(line, pathToFile);
+
+//                    foreach (var neighbourId in neighbours)
+//                    {
+//                        var neighbour = graph[neighbourId];
+//                        var edge = new Edge { Source = graph[nodeId],  Destination = neighbour };
+//                        graph[nodeId].Edges.Add(edge);
+//                    }
+//                }
+//            }
+
+//            return graph;
+//        }
+
+//        /// <summary>
+//        /// The check if file exists. 
+//        /// </summary>
+//        /// <param name="path">
+//        /// The path.
+//        /// </param>
+//        private void CheckIfFileExists(string path)
+//        {
+//            if (!File.Exists(path))
+//            {
+//                throw new GraphMatchingsException($"File \"{path}\" does not exist.");
+//            }
+//        }
+
+//        /// <summary>
+//        /// The parse number of Nodes from file.
+//        /// </summary>
+//        /// <param name="reader">
+//        /// The reader.
+//        /// </param>
+//        /// <param name="path">
+//        /// The path.
+//        /// </param>
+//        /// <returns>
+//        /// The <see cref="int"/>.
+//        /// </returns>
+//        private int ParseNumberOfNodesFromFile(StreamReader reader, string path)
+//        {
+//            var numberOfNodesLine = reader.ReadLine();
+//            if (string.IsNullOrEmpty(numberOfNodesLine))
+//            {
+//                throw new GraphMatchingsException($"First line in file \"{path}\" is empty. It should contain number of Nodes");
+//            }
+
+//            var isParseSuccessfull = int.TryParse(numberOfNodesLine, out var numberOfNodes);
+
+//            if (!isParseSuccessfull)
+//            {
+//                throw new GraphMatchingsException($"Could not parse first line \"{numberOfNodesLine}\" from file \"{path}\" to int. This line should represent number of Nodes.");
+//            }
+
+//            return numberOfNodes;
+//        }
+
+//        /// <summary>
+//        /// The read line from file.
+//        /// </summary>
+//        /// <param name="reader">
+//        /// The reader.
+//        /// </param>
+//        /// <param name="i">
+//        /// The i.
+//        /// </param>
+//        /// <param name="numberOfNodes">
+//        /// The number of Nodes.
+//        /// </param>
+//        /// <param name="pathToFile">
+//        /// The path to file.
+//        /// </param>
+//        /// <returns>
+//        /// The <see cref="string"/>.
+//        /// </returns>
+//        private string ReadLineFromFile(StreamReader reader, int i, int numberOfNodes, string pathToFile)
+//        {
+//            var line = reader.ReadLine();
+
+//            if (string.IsNullOrEmpty(line))
+//            {
+//                throw new GraphMatchingsException($"Line {i} in file {pathToFile} is null or empty. Expecting {numberOfNodes} lines.");
+//            }
+
+//            return line;
+//        }
+
+//        /// <summary>
+//        /// The parse node id from line.
+//        /// </summary>
+//        /// <param name="line">
+//        /// The line.
+//        /// </param>
+//        /// <param name="path">
+//        /// The path.
+//        /// </param>
+//        /// <returns>
+//        /// The <see cref="int"/>.
+//        /// </returns>
+//        private int ParseNodeIdFromLine(string line, string path)
+//        {
+//            var Nodes = line.Split(' ');
+//            var nodeNumberString = Nodes.First();
+
+//            var isParseSuccessfull = int.TryParse(nodeNumberString, out var nodeId);
+
+//            if (!isParseSuccessfull)
+//            {
+//                throw new GraphMatchingsException($"Could not parse first line \"{line}\" from file \"{path}\" to int. This line should represent number of Nodes.");
+//            }
+
+//            return nodeId;
+//        }
+
+//        /// <summary>
+//        /// The parse neighbours from file.
+//        /// </summary>
+//        /// <param name="line">
+//        /// The line.
+//        /// </param>
+//        /// <param name="path">
+//        /// The path.
+//        /// </param>
+//        /// <returns>
+//        /// The <see cref="List{T}"/>.
+//        /// </returns>
+//        private List<int> ParseNeighboursFromFile(string line, string path)
+//        {
+//            var splitedLine = line.Split(' ');
+//            var neighbours = new List<int>();
+
+//            // We are starting from 1 because first number in the line is the node and neighbours start from 1.
+//            for (int i = 1; i < splitedLine.Length; ++i)
+//            {
+//                var isParseSuccessfull = int.TryParse(splitedLine[i], out var nodeId);
+
+//                if (!isParseSuccessfull)
+//                {
+//                    throw new GraphMatchingsException($"Could not parse character \"{splitedLine[i]}\" from file \"{path}\" to int. This string should represent node.");
+//                }
+
+//                neighbours.Add(nodeId);
+//            }
+
+//            return neighbours;
+//        }
+//    }
+//}
