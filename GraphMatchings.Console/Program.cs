76dccc7d755a18bd7c1672f7f8c44cbc9479fba2
Jakub Mamelski <jakub.mamelski@outlook.com>
2019-01-22 14:32:50 +0100

    Remove old solution and add new, empty one

---
+++ GraphMatchings.Console/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GraphMatchings.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+
+        }
+    }
+}
