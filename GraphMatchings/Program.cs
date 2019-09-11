80dadd870f8f9dd83f1da09a734b5f051d55e2d5
Mamelski <jakub.mamelski@outlook.com>
2019-09-11 20:23:16 +0200

    up

--- GraphMatchings/Program.cs
+++ GraphMatchings/Program.cs
@@ -2,15 +2,23 @@
 {
     using CommandLine;
 
+    using GraphMatchings.Core;
+
     public class Program
     {
         public static void Main(string[] args)
         {
-            Parser.Default.ParseArguments<CommandLineOptions>(args).WithParsed(o => { ParseInputAndRunAlgorithm(o.InputFile); });
+            Parser.Default.ParseArguments<CommandLineOptions>(args)
+                .WithParsed(
+                    o =>
+                        {
+                            ParseInputAndRunAlgorithm(o.InputFile);
+                        });
         }
 
-        static void ParseInputAndRunAlgorithm(string pathToFile)
+        private static void ParseInputAndRunAlgorithm(string pathToFile)
         {
+            var graph = GraphParser.Parse(pathToFile);
             int b = 0;
         }
     }
