1b8fce34e5b00d82b851240d748a481ab2125a38
Mamelski <jakub.mamelski@outlook.com>
2019-09-14 19:31:49 +0200

    up

---
+++ GraphMatchings.Core/Utils/MatrixHelper.cs
@@ -0,0 +1,22 @@
+﻿namespace GraphMatchings.Core.Utils
+{
+    using System.Collections.Generic;
+
+    public static class GraphHelper
+    {
+        public static IEnumerable<int> GetNeighbours(int[,] graph, int node)
+        {
+            var neighbours = new List<int>();
+
+            for (var i = 0; i < graph.GetLength(1); ++i)
+            {
+                if (graph[node, i] != 0)
+                {
+                    neighbours.Add(i);
+                }
+            }
+
+            return neighbours;
+        }
+    }
+}
