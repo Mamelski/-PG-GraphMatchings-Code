6015be9f6434075c41b656e719703dc97bec5016
Mamelski <jakub.mamelski@outlook.com>
2019-09-18 22:05:04 +0200

    add options

--- GraphMatchings.GraphGenerator/CommandLineOptions.cs
+++ GraphMatchings.GraphGenerator/CommandLineOptions.cs
@@ -1,15 +1,37 @@
-﻿namespace GraphMatchings.Console
+﻿namespace GraphMatchings.GraphGenerator
 {
     using CommandLine;
 
     public class CommandLineOptions
     {
-        [Option('f',
-            "fileName",
+        [Option('g',
+            "numberOfGraphs",
             Required = true,
-            HelpText = "Input file containing connected graph. "
-                       + "First line should be the number of nodes (n). "
-                       + "The next following n lines should contain node number and then all its neighbour nodes numbers, all separated by spaces.")]
-        public string InputFile { get; set; }
+            HelpText = "Number of graphs to generate")]
+        public int NumberOfGraphs { get; set; }
+
+        [Option('n',
+            "numberOfNodes",
+            Required = true,
+            HelpText = "Number of nodes to generate in graph")]
+        public int NumberOfNodes { get; set; }
+
+        [Option('e',
+            "numberOfEdges",
+            Required = true,
+            HelpText = "Number of edges to generate in graph")]
+        public int NumberOfEdges { get; set; }
+
+        [Option('t',
+            "graphType",
+            Required = true,
+            HelpText = "Type of graph to generate. Can be \"W\" or \"N\"")]
+        public string GraphType { get; set; }
+
+        [Option('w',
+            "maximEdgeWeight",
+            Required = false,
+            HelpText = "Maximum weight of generated edge")]
+        public int MaximumEdgeWeight { get; set; }
     }
 }
