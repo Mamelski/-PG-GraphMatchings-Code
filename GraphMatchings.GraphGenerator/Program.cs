eb27d7052749f4a19167e3e1563f5d53467b6b76
Mamelski <jakub.mamelski@outlook.com>
2019-09-20 10:11:18 +0200

    generate up to adjacent done

--- GraphMatchings.GraphGenerator/Program.cs
+++ GraphMatchings.GraphGenerator/Program.cs
@@ -14,40 +14,36 @@
 
         private static bool isWeighted;
 
-       // private static string NautyFolder = @"C:\Users\Jakub\Documents\GitHub\-PG-GraphMatchings-Code\nauty26r10\";
+        private static string nautyFolder = @"C:\Users\Jakub\Documents\GitHub\-PG-GraphMatchings-Code\nauty26r10\";
+
+        private static string graph6FormatDirectory = @"Bigraphs\Graph6Format\";
+
+        private static string adjacencyMatrixFormatDirectory = @"Bigraphs\AdjacencyMatrixFormat\";
 
         public static void Main(string[] args)
         {
-            //Parser.Default.ParseArguments<CommandLineOptions>(args)
-            //    .WithParsed(
-            //        o =>
-            //            {
-            //                ParseInputAndRunAlgorithm(o);
-            //            });
-
             GenerateAllBipartiteConnectedGraphsWithMax10Nodes();
+            ChangeGeneratedBipartiteConnectedGraphsWithMax10NodesToVisibleFormat();
         }
 
         private static void GenerateAllBipartiteConnectedGraphsWithMax10Nodes()
         {
-            var nautyFolder = @"C:\Users\Jakub\Documents\GitHub\-PG-GraphMatchings-Code\nauty26r10\";
-
             for (var i = 1; i <= 10; ++i)
             {
                 for (var j = i; i + j <= 10; ++j)
                 {
-                    var command = $"genbg -c {i} {j} > \"Bigraphs\\Graph6Format\\{i}-{j}.txt\"";
+                    var command = $"genbg -c {i} {j} > {graph6FormatDirectory}{i}-{j}.txt";
 
                     var cmdProcess = new Process
                     {
                         StartInfo =
                         {
                             FileName = "cmd.exe",
+                            Arguments = $"/C {nautyFolder}{command}",
                             RedirectStandardInput = true,
                             RedirectStandardOutput = true,
                             CreateNoWindow = true,
-                            UseShellExecute = false,
-                            Arguments = $"/C {nautyFolder}{command}"
+                            UseShellExecute = false
                         }
                     };
 
@@ -59,6 +55,36 @@
                 }
             }
         }
+
+        private static void ChangeGeneratedBipartiteConnectedGraphsWithMax10NodesToVisibleFormat()
+        {
+            foreach (var filePath in Directory.GetFiles(graph6FormatDirectory))
+            {
+                var fileName = Path.GetFileName(filePath);
+                var command =
+                    $"showg -a {graph6FormatDirectory}{fileName} > {adjacencyMatrixFormatDirectory}{fileName}\"";
+
+                var cmdProcess = new Process
+                                     {
+                                         StartInfo =
+                                             {
+                                                 FileName = "cmd.exe",
+                                                 Arguments = $"/C {nautyFolder}{command}",
+                                                 RedirectStandardInput = true,
+                                                 RedirectStandardOutput = true,
+                                                 CreateNoWindow = true,
+                                                 UseShellExecute = false
+                                             }
+                                     };
+
+                cmdProcess.Start();
+
+                cmdProcess.StandardInput.Flush();
+                cmdProcess.StandardInput.Close();
+                cmdProcess.WaitForExit();
+            }
+        }
+
 
         private static void ParseInputAndRunAlgorithm(CommandLineOptions options)
         {
